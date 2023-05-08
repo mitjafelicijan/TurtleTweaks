3891dc57356d4e003bd69d41ca94a56a1aaf3462
Mitja Felicijan <mitja.felicijan@gmail.com>
2023-05-08 09:52:09 +0200

    Renamed remaining strings to new name

--- settings.lua
+++ settings.lua
@@ -2,7 +2,7 @@ local maxWidth = 500
 local maxHeight = 400 -- 280
 local alreadyLoaded = false
 
-local settings = CreateFrame("Frame", "TurtleTweaksGUI", UIParent)
+local settings = CreateFrame("Frame", "VanillaTweaksGUI", UIParent)
 
 settings:RegisterEvent("ADDON_LOADED")
 
@@ -27,8 +27,7 @@ settings:SetScript("OnEvent", function()
         })
 
         -- Add a title to the frame.
-        -- AdvancedSettingsGUITtitle
-        settings.title = CreateFrame("Frame", "TurtleTweaksGUITitle", settings)
+        settings.title = CreateFrame("Frame", "VanillaTweaksGUITitle", settings)
         settings.title:SetPoint("TOP", settings, "TOP", 0, 12)
         settings.title:SetWidth(256)
         settings.title:SetHeight(64)
@@ -40,11 +39,11 @@ settings:SetScript("OnEvent", function()
 
         -- Create a font string for the title.
         settings.title.text = settings.title:CreateFontString(nil, "HIGH", "GameFontNormal")
-        settings.title.text:SetText("Turtle Tweaks")
+        settings.title.text:SetText("Vanilla Tweaks")
         settings.title.text:SetPoint("TOP", 0, -14)
 
         -- Creates a cancel button that is attached to our frame.
-        settings.cancel = CreateFrame("Button", "TurtleTweaksGUICancel", settings, "GameMenuButtonTemplate")
+        settings.cancel = CreateFrame("Button", "VanillaTweaksGUICancel", settings, "GameMenuButtonTemplate")
         settings.cancel:SetWidth(90)
         settings.cancel:SetPoint("BOTTOMRIGHT", settings, "BOTTOMRIGHT", -17, 17)
         settings.cancel:SetText(CANCEL)
@@ -61,7 +60,7 @@ settings:SetScript("OnEvent", function()
         end)
 
         -- Creates a okay button that is attached to our frame.
-        settings.okay = CreateFrame("Button", "TurtleTweaksGUIOkey", settings, "GameMenuButtonTemplate")
+        settings.okay = CreateFrame("Button", "VanillaTweaksGUIOkey", settings, "GameMenuButtonTemplate")
         settings.okay:SetWidth(90)
         settings.okay:SetPoint("RIGHT", settings.cancel, "LEFT", 0, 0)
         settings.okay:SetText(OKAY)
@@ -78,7 +77,7 @@ settings:SetScript("OnEvent", function()
         end)
 
         -- Creates a defaults button that is attached to our frame.
-        settings.defaults = CreateFrame("Button", "TurtleTweaksGUIDefaults", settings, "GameMenuButtonTemplate")
+        settings.defaults = CreateFrame("Button", "VanillaTweaksGUIDefaults", settings, "GameMenuButtonTemplate")
         settings.defaults:SetWidth(90)
         settings.defaults:SetPoint("BOTTOMLEFT", settings, "BOTTOMLEFT", 17, 17)
         settings.defaults:SetText(DEFAULTS)
@@ -95,9 +94,10 @@ settings:SetScript("OnEvent", function()
         end)
 
         -- Add a button to the game menu that opens our addon frame when clicked.
-        local mainMenuButton = CreateFrame("Button", "TurtleTweaksGUIMenuButton", GameMenuFrame, "GameMenuButtonTemplate")
+        local mainMenuButton = CreateFrame("Button", "VanillaTweaksGUIMenuButton", GameMenuFrame,
+            "GameMenuButtonTemplate")
         mainMenuButton:SetPoint("TOP", GameMenuButtonUIOptions, "BOTTOM", 0, -1)
-        mainMenuButton:SetText("Turtle Tweaks")
+        mainMenuButton:SetText("Vanilla Tweaks")
         mainMenuButton:SetScript("OnClick", function()
             HideUIPanel(GameMenuFrame)
             settings:Show()
@@ -109,7 +109,7 @@ settings:SetScript("OnEvent", function()
         GameMenuFrame:SetHeight(GameMenuFrame:GetHeight() + 10)
 
         -- Create a container for our settings.
-        settings.container = CreateFrame("Frame", "TurtleTweaksGUIContainer", settings)
+        settings.container = CreateFrame("Frame", "VanillaTweaksGUIContainer", settings)
         settings.container:SetPoint("CENTER", settings, 0, 20)
         settings.container:SetHeight(maxHeight - 30)
         settings.container:SetWidth(maxWidth - 20)
