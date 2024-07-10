7aa6455c70c1774d5df5ea710d3e3a174641e65b
Mitja Felicijan <mitja.felicijan@gmail.com>
2024-07-11 00:10:30 +0200

    Castbar position listens to all events

--- CastbarPosition.lua
+++ CastbarPosition.lua
@@ -7,15 +7,7 @@ local feature = ns.Register({
 
 local frame = CreateFrame("Frame")
 
-frame:RegisterEvent("ADDON_LOADED")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:RegisterEvent("UI_SCALE_CHANGED")
-frame:RegisterEvent("DISPLAY_SIZE_CHANGED")
-frame:RegisterEvent("UNIT_SPELLCAST_SENT")
-frame:RegisterEvent("SPELL_UPDATE_USABLE")
-frame:RegisterEvent("UNIT_AURA")
-frame:RegisterEvent("SPELLCAST_START")
-frame:RegisterEvent("CURRENT_SPELL_CAST_CHANGED")
+frame:RegisterAllEvents()
 
 frame:SetScript("OnEvent", function()
   if not ns.IsEnabled(feature.identifier) then return end
